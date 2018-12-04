eab55287f86b03fac8cd157959d825c1442827aa
root <root@ubuntu.ubuntu-domain>
2018-12-04 22:30:11 +0800

    30 commit

---
+++ test/examples100/example22.py
@@ -0,0 +1,10 @@
+import sys
+for i in range(ord('x'), ord('z') + 1):
+    for j in range(ord('x'), ord('z') + 1):
+        if i != j:
+            for k in range(ord('x'), ord('z') + 1):
+                if (i != k) and (j != k):
+                    if (i != ord('x')) and (k != ord('x')) and (k != ord('z')):
+                        print 'a vs %s -- b vs %s -- c vs %s' %(chr(i), chr(j), chr(k))
+
+
