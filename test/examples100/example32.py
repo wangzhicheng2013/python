996b9644c26e81bfe24e03c394999efded4ed042
root <root@ubuntu.ubuntu-domain>
2018-12-23 21:51:37 +0800

    40 commit

---
+++ test/examples100/example32.py
@@ -0,0 +1,6 @@
+import sys
+L = [1, 2, 3]
+print L
+print L[::-1]
+for i in range(-1, -4, -1):
+    print L[i],
---
+++ test/examples100/example33.py
@@ -0,0 +1,6 @@
+import sys
+L = [1, 2, 3, 4, 5, 6, 7, 8]
+LL = ','.join(str(ch) for ch in L)
+print L
+print LL
+    
