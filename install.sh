ad700f9057d8b72c493dd8fddce687e58f2b1efc
root <root@ubuntu.ubuntu-domain>
2018-12-28 21:29:54 +0800

    44 commit

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #echo "# python" >> README.md
 #git init
 git add *
-git commit -m "43 commit"
+git commit -m "44 commit"
 #git remote add origin https://github.com/wangzhicheng2013/python.git
 git push -u origin master
---
+++ test/examples100/example37.py
@@ -0,0 +1,10 @@
+import sys
+if __name__ == '__main__':
+    L = []
+    for i in range(10):
+        number = int(raw_input("number = "))
+        L.append(number)
+    L.sort();
+    print L
+    
+    
