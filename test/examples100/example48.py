a8753c41f40299aaac87fd2f6fc2f4332143000d
root <root@ubuntu.ubuntu-domain>
2019-01-21 21:45:20 +0800

    54 commit

---
+++ test/examples100/example48.py
@@ -0,0 +1,10 @@
+import sys
+if __name__ == '__main__':
+    i = 10
+    j = 20
+    if i > j:
+        print '%d > %d' %(i, j)
+    elif i == j:
+        print '%d == %d' %(i, j)
+    else:
+        print '%d < %d' %(i, j)
