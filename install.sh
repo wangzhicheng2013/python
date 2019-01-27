3e0f63c391710c751c16454c86ae5c3f86376cdd
root <root@ubuntu.ubuntu-domain>
2019-01-27 21:46:54 +0800

    59 commit

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #echo "# python" >> README.md
 #git init
 git add *
-git commit -m "58 commit"
+git commit -m "59 commit"
 #git remote add origin https://github.com/wangzhicheng2013/python.git
 git push -u origin master
---
+++ test/examples100/example54.py
@@ -0,0 +1,6 @@
+import sys
+a = 1878
+a = a >> 4
+print a
+a = a | 15
+print a
