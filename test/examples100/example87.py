dcd524ec4aa987a0a9cd0b0ae5e3a22975575865
root <root@ubuntu.ubuntu-domain>
2019-02-22 21:36:00 +0800

    74 commit

---
+++ test/examples100/example87.py
@@ -0,0 +1,11 @@
+import sys
+import keyword
+from sys import argv, path
+print keyword.kwlist
+total = "A" + \
+         "B" + \
+         "C"
+print total
+print total[0 : -1]
+print total * 10
+print path
